26a1969b4711bbe9d78d8cdb0155ebd36b8450b4
CodeArgha <154545215+codeargha@users.noreply.github.com>
2024-01-07 19:59:51 +0530

    Add files via upload

---
+++ 2-Exercise_find_Even_odd_number.py
@@ -0,0 +1,6 @@
+number = int(input())
+
+if number % 2 == 0:
+    print("This number is an even number.")
+else:
+    print("This number is an odd number")
---
+++ 5-exercise_leap_year.py
@@ -0,0 +1,8 @@
+year = int(input())
+
+''' 
+This is how we work out whether if a particular year is a leap year
+    * on every year that is devisable by 4 with no reminder.
+    * except every year that is evenly devisable by 100 with no reminder.
+    *unless the year is also devisable by 400 with no reminder.
+'''
